bfc12e886b75b18268bda78bed66ac44d364683a
Ítalo H. <italodssouza.backup@gmail.com>
2026-03-25 10:32:39 -0300

    First commit

---
+++ src/test/java/tech/kvothe/git_actions/GitActionsApplicationTests.java
@@ -0,0 +1,23 @@
+package tech.kvothe.git_actions;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+class GitActionsApplicationTests {
+
+	@Test
+	void pass() {
+		assertEquals(1, 1);
+	}
+
+	@Test
+	void fail() {
+		assertEquals(1, 2);
+	}
+
+}
